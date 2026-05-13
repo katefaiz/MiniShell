4a6c97d06d47167d772238fc9ce649f95d2ea40c
Зарина Шарипова <Sh.zarina.ild@gmail.com>
2026-05-13 08:37:54 +0300

    Updated man

--- run.sh
+++ run.sh
@@ -6,8 +6,8 @@ if [ ! -d "$BUILD_DIR" ]; then
     mkdir "$BUILD_DIR"
 fi
 
-cmake -S . -B "$BUILD_DIR"
-cmake --build "$BUILD_DIR"
+cmake -S . -B "$BUILD_DIR" > /dev/null 2>&1
+cmake --build "$BUILD_DIR" > /dev/null 2>&1
 
 if [ $? -eq 0 ]; then
     cd "$BUILD_DIR"
